c1166c02ba64027be7a5dbf2ee02351fd578a353
atharhashcash <athar@hashcashconsultants.com>
2025-10-28 02:53:24 +0530

    modified the chatbot

--- public/chat-widget.js
+++ public/chat-widget.js
@@ -508,9 +508,8 @@
                     width: 32px;
                     height: 32px;
                     border-radius: 50%;
-                    background: ${config.primaryColor};
                     display: flex;
-                    align-items: center;
+                    align-items: end;
                     justify-content: center;
                     flex-shrink: 0;
                 }
@@ -519,6 +518,13 @@
                     width: 18px;
                     height: 18px;
                     fill: ${config.textColor};
+                }
+
+                .runit-message-avatar img {
+                    width: 32px;
+                    height: 32px;
+                    border-radius: 50%;
+                    object-fit: cover;
                 }
 
                 .runit-message-content {
@@ -792,7 +798,7 @@
                 <div id="runit-chat-messages"></div>
                 ${config.branding?.showPoweredBy !== false ?
                     `<div class="runit-powered-by">
-                        Powered by <a href="https://labs.runit.in" target="_blank">RunIt AI</a>
+                        Powered by <a href="https://labs.ragsense.co" target="_blank">ragsense.co</a>
                     </div>` : ''}
                 <div id="runit-chat-input-container">
                     <input type="text" id="runit-chat-input" placeholder="${config.placeholderText}" />
@@ -922,21 +928,18 @@
             messageDiv.id = messageId;
         }
 
-        const avatarSvg = type === 'bot'
-            ? '<path d="M12 2C6.48 2 2 6.48 2 12c0 5.52 4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/>'
-            : '<path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>';
+        const avatarContent = type === 'bot'
+            ? '<img src="https://www.ragsense.co/favicon/favicon-96x96.png" alt="AI Assistant" />'
+            : '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>';
 
         const formattedContent = type === 'bot' ? formatMessage(content) : escapeHtml(content);
 
         messageDiv.innerHTML = `
             <div class="runit-message-avatar">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    ${avatarSvg}
-                </svg>
+                ${avatarContent}
             </div>
             <div>
                 <div class="runit-message-content">${formattedContent}</div>
-                ${type === 'bot' ? '<div class="runit-ai-badge">AI Assistant</div>' : ''}
             </div>
         `;
 
@@ -975,9 +978,7 @@
         typingDiv.id = 'runit-typing-indicator';
         typingDiv.innerHTML = `
             <div class="runit-message-avatar">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path d="M12 2C6.48 2 2 6.48 2 12c0 5.52 4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/>
-                </svg>
+                <img src="https://www.ragsense.co/favicon/favicon-96x96.png" alt="AI Assistant" />
             </div>
             <div class="runit-typing-indicator">
                 <div class="runit-typing-dot"></div>
